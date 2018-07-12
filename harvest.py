ebf8cb21ccdcc61ad6035cad090690475992c1d8
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-12 13:01:45 -0700

    create MelonType class and add melon functions

--- harvest.py
+++ harvest.py
@@ -13,8 +13,8 @@ class MelonType(object):
         self.code = code
         self.first_harvest = first_harvest
         self.color = color
-     	self.is_seedless = is_seedless
-     	self.is_bestseller = is_bestseller
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
         self.pairings = []
 
     def add_pairing(self, pairing):
@@ -33,19 +33,46 @@ def make_melon_types():
 
     all_melon_types = []
 
-    # Fill in the rest
+    muskmelon = MelonType("musk", "1998", "green", True, True, "Muskmelon")
+    muskmelon.add_pairing("mint")
+    all_melon_types.append(muskmelon)
+
+
+    casaba = MelonType("cas", "2003", "orange", False, False, "Casaba")
+    casaba.add_pairing("strawberies")
+    casaba.add_pairing("mint")
+    all_melon_types.append(casaba)
+
+    crenshaw = MelonType("cren", "1996", "green", False, False, "Crenshaw")
+    crenshaw.add_pairing("proscuitto")
+    all_melon_types.append(crenshaw)
+
+    yellow_watermelon = MelonType("yw", "2013", "yellow", False, True, "Yellow Watermelon")
+    yellow_watermelon.add_pairing("ice cream")
+    all_melon_types.append(yellow_watermelon)
 
     return all_melon_types
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
-    # Fill in the rest
+    for melon in melon_types:
+        print("{melon} pairs with".format(melon = melon.name))
+        for pair in melon.pairings:
+            print("- {pair}".format(pair = pair))
+        print("")
+
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
-    # Fill in the rest
+    melon_codes = {}
+    
+    for melon in melon_types:
+        melon_codes[melon.code] = melon
+
+    return melon_codes    
+
 
 ############
 # Part 2   #
@@ -69,3 +96,6 @@ def get_sellability_report(melons):
 
 
 
+all_melons = make_melon_types()
+print_pairing_info(all_melons)
+make_melon_type_lookup(all_melons)
